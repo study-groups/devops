7d7d2106bf85b7c0a1d030a2ff4122f3610a90e0
Mike Ricos via ux305 <mike.ricos@gmail.com>
2019-03-25 03:21:51 -0600

    Commits rm'd files.

--- doapps/webserver/run.sh
+++
@@ -1,9 +0,0 @@
-# run daemonized, iteractive with terminal output, map port 52022
-# to 
-
-docker run --name $app -d -it \
-	--restart always \
-	-v $htdocs:/usr/local/apache2/htdocs \
-	-p $appHttpPort:80 \
-	-p $appHttpsPort:443 \
-        $image
