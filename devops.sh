9c56c116cf5581324156c2ed9f9b545545c1609a
Mike Ricos <mike.ricos@gmail.com>
2020-02-26 22:47:49 -0800

    Added availability matrix.

--- devops.sh
+++ devops.sh
@@ -12,6 +12,8 @@ dotool-help(){
 
   https://www.digitalocean.com/community/tutorials/
   how-to-use-doctl-the-official-digitalocean-command-line-client
+
+  https://www.digitalocean.com/docs/platform/availability-matrix/
   "
 }
 
