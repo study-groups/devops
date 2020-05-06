761571c52db42243ee2d5fce107f6765c0adeeea
Mike Ricos <mike.ricos@gmail.com>
2020-05-06 01:02:51 -0700

    Frist draft of sync working.

--- sync.sh
+++ sync.sh
@@ -1,8 +1,10 @@
 #!/bin/env bash
 mode=live # anything but live will be a dryrun.
+SYNC="/home/mricos/src/study-groups/devops-study-group/sync.sh"
 
-TO="/mnt/volume_sfo2_02"
-TO_HOST="mricos@ux305-2.local"
+TO_MNT="/mnt/volume_sfo2_02"
+TO_USER="mricos"
+TO_HOST="ux305-2.local"
 TO_DIR="/home/mricos/backups"
 FROM=/home/mricos/files/
 
@@ -10,38 +12,45 @@ sync-help(){
   echo "\
 Sync is a collection shell functions for continual backup of unix servers."
 }
+
 sync-from-to() {
-  local from=$1 # full path
-  local to="$2"
-  to="$to/$HOSTNAME/$from"
+  local from_dir=$1 # full path
+  local to_user="$2"
+  local to_host="$3"
+  local to_dir="$4"
+  local from_hostname=$HOSTNAME
+  local to_path="$to_dir/$from_hostname/$from_dir"
+  local mkdircmd="ssh $to_user@$to_host mkdir -p $to_path"
+  local to_total="$to_user"@"$to_host":"$to_path"
   local params="-avzP" # archive,verbose,compress,Partial
-  cmd=$(echo rsync $params "$from" "$to")
-  echo $cmd
-  [ $mode = "xlive" ] && exec $cmd
+  cmd=$(echo rsync $params "$from_dir" "$to_total")
+  echo "$mkdircmd; $cmd" # copy and paste to execute
+  #[ $mode = "not-live" ] && exec $cmd    # not live
 }
 
 sync-from() {
-  local from=$1
-  local to="$TO"
-  sync-from-to $from $to
+  sync-from-to $1 $TO_USER $TO_HOST $TO_PATH
 }
 
 sync-to() {
-  local to=$1
-  local from="$FROM"
-  sync-from-to $from $to
+  local to_user=$1
+  local to_host=$2
+  local to_path=$3
+  sync-from-to $FROM $to_user $to_host $to_path
 }
+
 sync-all() {
   local from="$FROM"
-  sync-from-to $FROM "$TO_HOST:/$TO_DIR"
+  sync-from-to $FROM $TO_USER $TO_HOST $TO_DIR
 }
+
 sync-notes(){
   echo "
-Rsync is an important part of the interent:
+sync- relies on the master rsync:
 
   https://linux.die.net/man/1/rsync
 
-It works like rcp (remote copy) which works like cp.
+It works like rcp which works like cp.
 
 It currently does not implement in-memory synchronization.
 See man sync for that.
