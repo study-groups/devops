e5a5cf5631553faa84e1ca29e534d0670080d253
Zach Overhulser <zoverhulser@gmail.com>
2020-08-13 15:49:21 -0500

    Adds message in default app.sh.

--- nodeholder/ubuntu/buildpak/app.sh
+++ nodeholder/ubuntu/buildpak/app.sh
@@ -2,4 +2,5 @@
 while true; do
   date;  # write to stdout which daemonized wires to repo/nh/log
   sleep 5; # do nothing for 5 seconds
+  "You need to edit your nh/start file for your specific app."
 done
