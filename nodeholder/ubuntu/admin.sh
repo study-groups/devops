a8fde0493aa86811586ca07e77b906e27a51a336
Zach Overhulser <zoverhulser@gmail.com>
2020-11-07 05:22:40 -0600

    Renames and comments for new convention.

--- nodeholder/ubuntu/admin.sh
+++ nodeholder/ubuntu/admin.sh
@@ -1,6 +1,6 @@
-# File starts below.
-####################################################################
 # Source these functions as admin. 
+
+# Explains use of admin
 admin-help(){
   echo "\
    Admin is a collection of scripts to configure runtime operations.
@@ -16,37 +16,42 @@ admin-help(){
 
   Configures a Unix server and account for process 'containers'
   addresseble by system defined TCP sockets. 
-"
+  "
 }
 
+# logs commands used as admin
 admin-log(){
   local funcname=${FUNCNAME[1]} # get function that called this
   echo $(date +%s) $funcname $@ >> /home/admin/log
 }
 
-admin-create-node() {
-  local node_name="$1";
+# creates role(user) on local machine
+admin-create-role() {
+  local role="$1";
 
-  sudo adduser --disabled-password --gecos "" $node_name
+  sudo adduser --disabled-password --gecos "" $role
   
   # copy admin key from root to user, so that admin may have access
-  sudo mkdir /home/$node_name/.ssh
+  sudo mkdir /home/$role/.ssh
   sudo cp /home/admin/.ssh/authorized_keys \
-	  /home/$node_name/.ssh/authorized_keys
+	  /home/$role/.ssh/authorized_keys
 
   # create ownership and groups
-  sudo chown -R $node_name:$node_name /home/$node_name/.ssh
-  sudo chmod 0700 /home/$node_name/.ssh
-  sudo chmod 0600 /home/$node_name/.ssh/authorized_keys
-  sudo -u $node_name ssh-keygen -N "" -q -f /home/$node_name/.ssh/id_rsa \
-	  -C "$node_name-$(date +%Y-%m-%d)"
+  sudo chown -R $role:$role /home/$role/.ssh
+  sudo chmod 0700 /home/$role/.ssh
+  sudo chmod 0600 /home/$role/.ssh/authorized_keys
+  # creates new ssh-key
+  sudo -u $role ssh-keygen -N "" -q -f /home/$role/.ssh/id_rsa \
+	  -C "$role-$(date +%Y-%m-%d)"
 }
 
-admin-remove-node() {
-  local node_name="$1";
+# removes role and 
+# kills all current processes working under that role
+admin-remove-role() {
+  local role="$1";
 
-  sudo pkill -9 -u "$node_name"
-  sudo deluser --remove-home "$node_name" ## --backup --backup-to
+  sudo pkill -9 -u "$role"
+  sudo deluser --remove-home "$role" ## --backup --backup-to
 }
 
 # Directory creation time is self timestamped.
@@ -68,6 +73,8 @@ admin-create-port(){
 
 #https://stackoverflow.com/questions/3685970/\
 #check-if-a-bash-array-contains-a-value
+
+# deletes port file
 admin-delete-port(){
   admin-log $@
   [ -z $1 ] && admin-log "no port entered" && return -1
@@ -86,6 +93,7 @@ admin-delete-port(){
     || echo "false"
 }
 
+
 admin-create-key(){
   admin-log $@
   ssh-keygen -C $1 -f /home/admin/.ssh/$1
@@ -99,77 +107,70 @@ admin-add-key() {
   ssh-add $1
 }
 
+# clones app from repo onto local machine
+# provides permissions to specified role
 admin-create-app(){
   admin-log $@
-  local nodename="$1";
+  local role="$1";
   local repo_url="$2";
   local branch=${3:-"master"};
   local basename=$(basename $repo_url); # myapp.git
   basename=${basename%.*}; # myapp  (removes .git)
-  local app_name=${4:-$basename};
+  local app=${4:-$basename};
 
-  [ -z "$nodename" ] \
-	  && echo "Please provide name of node to use" && return 1
-  [ -z "$repo_url" ] \
-	  && echo "Please provide the repo from which to clone" && return 1
+  [ -z "$role" ] && 
+    echo "Please provide name of role to use" && 
+    return 1
 
-  [ -d "/home/$nodename/$app_name" ] && \
+  [ -z "$repo_url" ] && 
+    echo "Please provide the repo from which to clone" && 
+    return 1
+
+  [ -d "/home/$role/$app" ] && 
 	  echo "app dir exists, exiting" && return -1
   
   local port=$(admin-create-port)
   admin-log port=$port
 
-  sudo -u $nodename ssh -T -o StrictHostKeyChecking=no git@gitlab.com
-  sudo -u $nodename mkdir /home/$nodename/$app_name 
-  sudo -u $nodename git clone \
+  # signals to gitlab that it is authorized to clone from repo
+  sudo -u $role ssh -T -o StrictHostKeyChecking=no git@gitlab.com
+  # creates directory for application
+  sudo -u $role mkdir /home/$role/$app 
+  # clones application from specified branch into specified directory
+  sudo -u $role git clone \
 	  --single-branch \
 	  --branch $branch \
-	  $repo_url /home/$nodename/$app_name
+	  $repo_url /home/$role/$app
 
   # if it didn't clone correctly, remove the directory
-  [ $? -ne 0 ] && sudo -u $nodename rmdir /home/$nodename/$app_name \
-	  && echo "Application was unable to clone." \
-	  && return 0
+  [ $? -ne 0 ] && sudo -u $role rmdir /home/$role/$app && 
+    echo "Application was unable to clone." && 
+    return 0
 
   # if nh dir does not exist, copy dummy 
   # user should modify nh/start and check nh dir in.
-  [ ! -d "/home/$nodename/$app_name/nh" ] \
-    && sudo -u $nodename \
-        cp -r /home/admin/buildpak \
-              /home/$nodename/$app_name/nh
+  [ ! -d "/home/$role/$app/nh" ] && 
+    sudo -u $role \
+    cp -r /home/admin/buildpak \
+      /home/$role/$app/nh
 
-  sudo -u $nodename bash -c "echo $port > /home/$nodename/$app_name/nh/port"
-  admin-log created /home/$nodename/$app_name
+  sudo -u $role bash -c "echo $port > /home/$role/$app/nh/port"
+  admin-log created /home/$role/$app
 }
 
+# deletes application on local machine
 admin-delete-app(){
   admin-log $@
-  local nodename=$1
-  local app_name=$2
-  local port=$(cat /home/$nodename/$app_name/nh/port)
-  sudo -u $nodename rm -rf /home/$nodename/$app_name
+  local role=$1
+  local app=$2
+  local port=$(cat /home/$role/$app/nh/port)
+  sudo -u $role rm -rf /home/$role/$app
   echo "PORT: $port"
   admin-delete-port $port
 }
 
-admin-undo-init(){
-  if [[ $NODEHOLDER_ROLE == "child" ]]
-  then
-    app-stop
-    rm -rf $userdir/src
-    rm -rf $userdir/buildpak
-    rm $userdir/admin.sh
-  
-    return 0 
-  fi
-
-  echo "Aborting undo. NODEHOLDER_ROLE not child."
-}
-
 admin-monitor(){
   watch -n .5 '
-    echo "/etc/passwd:"
-    tail -5 /etc/passwd
     echo "/etc/group:"
     tail -5 /etc/group
     echo "ls /tmp:"
