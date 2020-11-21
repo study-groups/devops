a20581be37e82aaeb91aa781f541c07578a8b30c
Zach Overhulser <zoverhulser@gmail.com>
2020-11-21 16:02:31 -0600

    Serves static site.

--- nodeholder/ubuntu/buildpak/app.sh
+++ nodeholder/ubuntu/buildpak/app.sh
@@ -1,6 +1,1 @@
-#!/bin/bash
-while true; do
-  date;  # write to stdout which daemonized wires to repo/nh/log
-  sleep 5; # do nothing for 5 seconds
-  echo "You need to edit your nh/start file for your specific app."
-done
+node ./public/index.js
