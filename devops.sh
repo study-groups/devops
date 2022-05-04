38e4a47e835fbd03888df8d0666543f1676e62e1
Mike <mike.ricos@gmail.com>
2022-05-04 14:39:17 -0700

    added mkdoc notes

--- devops.sh
+++ devops.sh
@@ -8,6 +8,7 @@ EOF
 devops-new-mkdocs-project(){
   # need to be in a pyenv env.
   # pip install mkdocs
+  # pip install --upgrade python-markdown-math
   # https://www.mkdocs.org/getting-started/
   mkdocs new $1 
 }
