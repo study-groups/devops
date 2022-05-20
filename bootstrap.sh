752c6dc3617982c3bb975883862d94ca393bb651
Mike Ricos <mike.ricos@gmail.com>
2022-05-20 08:34:08 +0000

    Added bootstrap procedure

---
+++ bootstrap.sh
@@ -0,0 +1,33 @@
+KEY="~/.ssh/do_spring_rsa"
+
+# must run as root
+nh-bootstrap(){
+#ssh-keygen
+#dnf install git # fedora
+#apt install git # ubuntu
+#eval "$(ssh-agent)"
+#ssh-add ~/.ssh/$KEY
+#mkdir src
+#cd src
+#git clone git@github.com:study-groups/devops-study-group.git
+#cd devops-study-group
+envsubst < ./custom.env > ~/custom.sh
+}
+
+nh-root-help(){
+cat <<EOF
+
+ Nodeholder bootstrap procedure:
+  0) set env variables used in custom.env
+  1) Make sure public key is in GitHub user account (any in group). 
+  2) This file needs to be manually copied from the repo.
+  3) Edit the root.sh copy on new host as necessary.
+  4) Sourced into a bash shell on the new host.
+  5) nh-root-install
+
+ Next steps:
+  1) source /root/custom.sh
+  2) 
+ 
+EOF
+}
