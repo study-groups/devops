041ba49e65e4e70e34ab9ebb3445bc8ffa42aeaf
Mike Ricos <mike.ricos@gmail.com>
2022-07-27 01:56:43 +0000

    Adds enc/decryption on stdio

--- tetra/bash/encrypt.sh
+++ tetra/bash/encrypt.sh
@@ -1,10 +1,17 @@
-# This file is in main.
 tetra-encrypt(){
   openssl aes-256-cbc -a -salt -in $1 -out $1.enc
 }
 
 tetra-decrypt(){
   openssl aes-256-cbc -d -a -in $1 -out $2 
+}
+
+tetra-encrypt-stdio(){
+  openssl aes-256-cbc -a -salt 
+}
+
+tetra-decrypt-stdio(){
+  openssl aes-256-cbc -a -d
 }
 
 tetra-htpasswd-set(){
---
+++ tetra/bash/tetra.sh
@@ -0,0 +1,23 @@
+#miscellaneous functions to be placed in proper file later
+
+# modern ubuntu uses snap,
+# lets hope the trouble is worth it..
+
+tetra-df(){
+  df -h | grep -v snap # filter out snap mounts
+}
+
+tetra-df-snap(){
+  df -h
+}
+
+# must be in reports dir
+tetra-encrypt-report(){
+   tar ca audit.txt summary.html | \
+   tetra-encrypt-stdio  > \
+   report_$(date +%s).tar.enc
+}
+
+tetra-decrypt-report(){
+   cat $1 | tetra-decrypt-stdio |tar xv
+}
