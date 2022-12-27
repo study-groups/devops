fc0b5d8ebdbc481b7045bc21a5ce1ac3e32a7294
Mike Ricos <mike.ricos@gmail.com>
2022-12-27 07:33:53 -0800

    Adds example of east/west

--- tetra/bash/env.sh
+++ tetra/bash/env.sh
@@ -1,5 +1,6 @@
 #  Init code for bootstrapping tetra into a users' Bash shell.
 
-[ -f tetra.env ] && source tetra.env
+# [ -f tetra.env ] && source tetra.env
 remoteEast="ssh admin@$do1"
 remoteWest="ssh admin@$do2"
+TETRA_PRIMARY_IP="$do4_n2"
