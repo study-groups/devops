c7dee3ef108fc7c9988da4d366347a1d3e158c03
Mike Ricos <mike.ricos@gmail.com>
2023-03-20 20:38:00 -0700

    Uses recommended way to get dir

--- tetra/bash/bootstrap.sh
+++ tetra/bash/bootstrap.sh
@@ -1,7 +1,7 @@
 # grep -v means ignore
-thisfile=${BASH_SOURCE[0]}
-realpath=$(readlink -f $thisfile)
-dirname=$(dirname $realpath)
+thisfile="${BASH_SOURCE[0]}"
+tetra_dir="$(cd "$(dirname $thisfile)" && pwd)"
+
 echo "In $thisfile"
-echo "Dirname: $dirname"
-for f in $(ls $dirname/*.sh | grep -v $thisfile); do source $f; done;
+echo "tetra_dir: $tetra_dir"
+for f in $(ls $tetra_dir/*.sh | grep -v $thisfile); do source $f; done;
