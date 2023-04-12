69746f6a5f6c80f361f02448233030fae8ad14f5
Mike Ricos <mike.ricos@gmail.com>
2023-04-11 21:51:39 -0700

    Final logtime format

--- tetra/bash/log.sh
+++ tetra/bash/log.sh
@@ -1,14 +1,17 @@
 tetra_bufsize=${tetra_bufsize:-1000}            # set/use system default
 tetra_logfile=${tetra_logfile:-/tmp/tetra.log}  # set/use system default
 
+# Apple_Date-T-UTC_time.MilliSecond-Z
+# date -u +"%FT%T.%3NZ"
+# 2023-04-12T03:58:38.352Z
+
 tetra-log(){
   local bufsize=$tetra_bufsize                    # easier to read below
   local logfile=$tetra_logfile                    # easier to read/resuse
   touch $logfile                                  # Create if it doesn't exist
   tail -n $bufsize $logfile > /dev/null           # Set up the circular buffer
-
   while read input; do
-      echo "$input" >> $logfile                   # to the log file
+  echo "$(date -u +"%FT%T.%3NZ") $input" >> $logfile  # to the log file
       tail -n $bufsize $logfile > $logfile.tmp    # Trim log to buffer size
       mv $logfile.tmp $logfile
   done
