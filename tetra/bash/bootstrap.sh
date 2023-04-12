57429f47393b92339b8b6bb80ad68f2ee96c8f05
Mike Ricos <mike.ricos@gmail.com>
2023-04-11 20:40:54 -0700

    Adds circular buffer log

--- tetra/bash/bootstrap.sh
+++ tetra/bash/bootstrap.sh
@@ -1,7 +1,37 @@
-# grep -v means ignore
-thisfile="${BASH_SOURCE[0]}"
-tetra_dir="$(cd "$(dirname $thisfile)" && pwd)"
+thisfile="${BASH_SOURCE[0]}"    
+tetra_src="$(cd "$(dirname $thisfile)" && pwd)"     #
+for f in $(ls $tetra_src/*.sh | grep -v $thisfile);
+  do source $f;
+done;
 
-echo "In $thisfile"
-echo "tetra_dir: $tetra_dir"
-for f in $(ls $tetra_dir/*.sh | grep -v $thisfile); do source $f; done;
+# tetra relies on user supplied PEM keys and env configuration files
+# TETRA_DIR is a global path containing organizational directories
+# to keep track of keys and env setups specific to an organization
+# and the sub systsems within.
+
+# if TETRA_DIR is set, skip
+# if not set it to $1 or default to ~/tetra
+if [ -z "$TETRA_DIR" ]; then
+    if [ -n "$1" ]; then
+        TETRA_DIR="$1"
+    else
+        TETRA_DIR="$HOME/tetra"
+    fi
+fi
+
+echo using $TETRA_DIR | tetra-log
+
+for d in $TETRA_DIR/*/; do
+    echo $d | tetra-log
+    if [ -e "$d/tetra.sh" ]; then
+        source "$d/tetra.sh"
+    fi
+done
+
+cat <<EOF
+
+  tetra_src: $tetra_src
+  TETRA_DIR: $TETRA_DIR
+  Tetra Bootstraping complete, tetra-logs to see more.
+
+EOF
---
+++ tetra/bash/log.sh
@@ -0,0 +1,18 @@
+tetra_bufsize=${tetra_bufsize:-10}              # set/use system default
+tetra_logfile=${tetra_logfile:-/tmp/tetra.log}  # set/use system default
+
+tetra-log(){
+  local bufsize=$tetra_bufsize                    # easier to read below
+  local logfile=$tetra_logfile                    # easier to read/resuse
+  touch $logfile                                  # Create if it doesn't exist
+  tail -n $bufsize $logfile > /dev/null           # Set up the circular buffer
+
+  while read input; do
+      echo "$input" >> $logfile                   # to the log file
+      tail -n $bufsize $logfile > $logfile.tmp    # Trim log to buffer size
+      mv $logfile.tmp $logfile
+  done
+}
+tetra-logs(){
+  cat $tetra_logfile                              # use system default
+}
