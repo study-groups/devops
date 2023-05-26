89417a8d2deb5cf154d016b0a172359fb8f0c56a
Mike Ricos <mike.ricos@gmail.com>
2023-05-26 01:06:12 -0700

    Safer bootstrapping

--- tetra/bash/bootstrap.sh
+++ tetra/bash/bootstrap.sh
@@ -5,30 +5,25 @@ thisfile=bootstrap.sh
 # to keep track of keys and env setups specific to an organization
 # and the sub systsems within.
 
-tetra_log(){
+tetra-log(){
    cat > /dev/null
 }
 
 # if TETRA_DIR is set, skip
 # if not set it to $1 or default to ~/tetra
 if [ -z "$TETRA_DIR" ]; then
-    if [ -n "$1" ]; then
-        TETRA_DIR="$1"
-    else
-        TETRA_DIR="$HOME/tetra"
-    fi
+    echo "TETRA_DIR not set"
+    return 1;
 fi
 
 [ -z "$TETRA_DIR/tetra.env" ] && source $TETRA_DIR/tetra.env
 
 
-# THIS IS BROKEN
-tetra_src="$(cd "$(dirname $thisfile)" && pwd)"
-echo tetra_source: $tetra_src
+tetra_src=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+echo tetra_src: $tetra_src
 for f in $(ls $tetra_src/*.sh | grep -v $thisfile);
   do source $f;
 done;
-
 
 echo using $TETRA_DIR | tetra-log
 
