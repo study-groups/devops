6a3bf0015a74df0896a28628ba876e8e70a4970e
Mike Ricos <mricos@m2.lan>
2023-06-04 01:51:03 -0700

    Using TETRA_SRC

--- tetra/bash/bootstrap.sh
+++ tetra/bash/bootstrap.sh
@@ -1,52 +1,46 @@
-thisfile=bootstrap.sh
-
 # tetra relies on user supplied PEM keys and env configuration files
 # TETRA_DIR is a global path containing organizational directories
 # to keep track of keys and env setups specific to an organization
 # and the sub systsems within.
 
+# Restart log
+tetralog=$TETRA_DIR/log.txt
+
+date > $tetralog
+
 tetra-log(){
-   #cat > /dev/null
-   cat 
+   # cat > /dev/null
+   cat  >> $tetralog 
 }
 
 # if TETRA_DIR is set, skip
 # if not set it to $1 or default to ~/tetra
 if [ -z "$TETRA_DIR" ]; then
-    echo "TETRA_DIR not set"
+    echo "TETRA_DIR not set" 
     return 1;
 fi
 
+if [ -z "$TETRA_SRC" ]; then
+    echo "TETRA_SRC not set"
+    return 1;
+fi
+
+echo TETRA_SRC: $TETRA_SRC | tetra-log
+echo TETRA_DIR: $TETRA_DIR | tetra-log
 [ -z "$TETRA_DIR/tetra.env" ] && source $TETRA_DIR/tetra.env
 
 
-tetra_src=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-echo tetra_src: $tetra_src
-for f in $(ls $tetra_src/*.sh | grep -v $thisfile);
+for f in $(ls $TETRA_SRC/*.sh | grep -v bootstrap.sh);
   do source $f;
 done;
 
-echo using $TETRA_DIR | tetra-log
-
-#for d in $TETRA_DIR/*/; do
-#    if [ -e "$d/tetra.sh" ]; then
-#        echo "sourcing $d" | tetra-log
-#        source "$d/tetra.sh"
-#    fi
-#done
-echo USING $TETRA_DIR
 for file in $(find $TETRA_DIR -type f -name "tetra.sh"); do
-    source "$file"
+    echo "sourcing $file" | tetra-log
+    source  "$file"
 done
 
-
-[ -z "$TETRA_DIR/tetra.env" ] && source $TETRA_DIR/tetra.env
-[ -f $TETRA_DIR/tetra.sh ] && source $TETRA_DIR/tetra.sh
-
 cat <<EOF
-
-  tetra_src: $tetra_src
+  TETRA_SRC: $TETRA_SRC
   TETRA_DIR: $TETRA_DIR
   Tetra Bootstraping complete, tetra-logs to see more.
-
 EOF
