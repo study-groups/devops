b9e7fa0735ad1385e2df71ca4f443efd2f2e6e1c
Mike Ricos <mike@lenan.net>
2023-12-12 04:20:06 -0800

    Adds tetra-tmux-tetra

--- tetra/bash/tetra.sh
+++ tetra/bash/tetra.sh
@@ -1,24 +1,33 @@
 #miscellaneous functions to be placed in proper file later
-tetra_df(){
-  df -h | grep -v snap # filter out snap mounts
+
+tetra-status(){
+   clear
+   tetra-df
 }
 
-tetra_df_snap(){
+tetra-df(){
+  df -h | grep -v -e snap -e tmp -e udev -e boot # filter out snap mounts
+}
+
+tetra-df-snap(){
   df -h
 }
 
 # must be in reports dir
-tetra_encrypt_report(){
+tetra-encrypt-report(){
    tar ca audit.txt summary.html | \
    tetra_encrypt_stdio  > \
    report_$(date +%s).tar.enc
 }
 
-tetra_decrypt_report(){
-   cat $1 | tetra_decrypt_stdio |tar xv
+tetra-decrypt-report(){
+   cat $1 | tetra-decrypt-stdio |tar xv
 }
 
-tetra_list_sites()
+####                          ###
+####  Move below to nginx.sh  ###
+####                          ###
+tetra-list-sites()
 {
   cat /etc/nginx/sites-enabled/* \
 	 | grep " server_name "  \
@@ -27,12 +36,12 @@ tetra_list_sites()
 	 | uniq 
 }
 
-tetra_install_crossplane(){
-# must have python enabled via tetra-python-activate
-pip install crossplane
+tetra-install-crossplane(){
+  # must have python enabled via tetra-python-activate
+  pip install crossplane
 }
 
 tetra_parse_nginx(){
-crossplane parse /etc/nginx/sites-enabled/*nodeholder*
+  crossplane parse /etc/nginx/sites-enabled/*nodeholder*
 }
 
--- tetra/bash/tmux.sh
+++ tetra/bash/tmux.sh
@@ -1,22 +1,58 @@
-tetra_tmux_new(){
+tetra-tmux-new(){
   local sessionName="${1:-"untitled-session"}
   local scriptName="${2:-"/dev/null"}
   tmux new-session -d -s "$sessionName" "$scriptName"
 }
 
-tetra_tmux_list(){
+tetra-tmux-list(){
   tmux list-sessions # aka tmux ls
 }
 
 # creates session without starting a script
-tetra_tmux_join(){
+tetra-tmux-join(){
   #tmux attach -t $1     
   tmux attach-session -d -t $1 # -d resizes to screen
   # $? is result of attempt to attach
   [ $? == 1 ] && tmux new -s $1 
 }
 
-tetra_tmux_kill(){
+tetra-tmux-kill(){
   tmux kill-session -t $1
 }
 
+tetra-tmux-tetra-kill(){
+  tmux kill-session -t tetra 
+}
+
+tetra-tmux-tetra(){
+  tmux kill-session -t tetra 
+  tmux new-session -d -s tetra
+
+  # Split the window into two panes horizontally
+  tmux split-window -v
+
+  # Select the first pane and split it vertically
+  tmux select-pane -t 0 
+  tmux split-window -h
+
+  # Export necessary variables
+  export do1
+  export do4_n2
+
+  # After SSHing into the first machine
+  tmux send-keys -t 0 'ssh root@$do1' C-m
+  tmux send-keys -t 0 'clear' C-m
+
+  # After SSHing into the second machine
+  tmux send-keys -t 1 'ssh root@$do4_n2' C-m
+  tmux send-keys -t 1 'clear' C-m
+
+  tmux send-keys -t 2 'TETRA_SRC=$HOME/src/devops-study-group/tetra/bash' C-m
+  tmux send-keys -t 2 'source $TETRA_SRC/bootstrap.sh' C-m
+  tmux send-keys -t 2 'tetra-status' C-m
+  tmux select-pane -t 2 
+
+  tmux set -g mouse on
+
+  tmux attach-session -t tetra
+}
