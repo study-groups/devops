cf641c18ed69d2331559b2080edde745f92f6708
Mike Ricos <mike@lenan.net>
2023-12-12 23:49:05 -0800

    Remove subshell

--- tetra/bash/status.sh
+++ tetra/bash/status.sh
@@ -1,10 +1,12 @@
 tetra-status(){
-   TETRA_REPO=$HOME/src/devops-study-group;
-   (cd $TETRA_REPO; git pull)
-   source $TETRA_SRC/bootstrap.sh
-   clear
-   uptime | awk -F 'load average:' '{print "load (1, 5, 15m)" $2}'
-   tetra-df
+    TETRA_REPO=$HOME/src/devops-study-group
+    pushd $TETRA_REPO
+    git pull
+    popd
+    source $TETRA_SRC/bootstrap.sh
+    clear
+    uptime | awk -F 'load average:' '{print "load (1, 5, 15m)" $2}'
+    tetra-df
 }
 
 tetra-df(){
