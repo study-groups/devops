2512f9d3c95e8c1186a36cc5f6e81d1ecc493226
Mike Ricos <mike.ricos@gmail.com>
2024-03-12 00:09:37 -0700

    Adds free for mac

--- tetra/bash/aliases.sh
+++ tetra/bash/aliases.sh
@@ -1,3 +1,16 @@
 alias ttks='tetra-tmux-kill-server'
 alias ttt='tetra-tmux-tetra'
+alias tttk='tetra-tmux-kill-session tetra'
 alias tnlr='tetra_nginx_location_replace'
+
+if [[ "$(uname)" == "Darwin" ]]; then
+    # macOS uses a different syntax for 'date'
+    alias date='gdate'
+    alias free='top -l 1 | grep PhysMem'
+else
+    # Linux and other Unix-like systems
+    echo Standard Linux
+    alias date=date
+    alias pbcopy='xclip -selection clipboard'
+    alias pbpaste='xclip -selection clipboard -o'
+fi
