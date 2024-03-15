b2f3d2ca75df2c2c129050845e5cca7b764edba6
Mike Ricos <mike.ricos@gmail.com>
2024-03-14 21:53:46 -0700

    Adds tmux panel indexing

--- tetra/bash/nvim.sh
+++ tetra/bash/nvim.sh
@@ -1,17 +1,40 @@
-tetra-nvim-clean-mac(){
+
+tetra_nvim_clean_mac(){
   brew uninstall nvim
-  rm -rf ~/.config/nvim
-  rm -rf ~/.local/share/nvim
-  rm -rf ~/.cache/nvim
-  rm -rf ~/.local/state/nvim
-  rm -rf ~/.local/share/nvim
+  tertra_nvim_clean_config
+}
+tetra_nvim_clean_config(){
+  rm -rf $HOME/.config/nvim
+  rm -rf $HOME/.local/share/nvim
+  rm -rf $HOME~/.cache/nvim
+  rm -rf $HOME~/.local/state/nvim
+  rm -rf $HOME~/.local/share/nvim
 }
 
-tetra-nvim-install-mac(){
+tetra_nvim_install_mac(){
   brew install nvim
+  tetra_nvim_install_pluginmgr
+}
+tetra_nvim_install_pluginmgr(){
   # packer is Lua based package mgr, works parallel to 
   # native Neovim package manager  ~/.config/nvim/pack
   # Packer uses  ~/.config/nvim/init.vim
-  git clone --depth 1 https://github.com/wbthomason/packer.nvim\
- ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+  git clone --depth 1 https://github.com/wbthomason/packer.nvim \
+ $HOME/.local/share/nvim/site/pack/packer/start/packer.nvim
+}
+
+tetra_nvim_install_telescope(){
+echo "In ~/./config/nvim/init.lua"
+echo "Add to packer.startup(function(use):"
+cat <<'EOF'
+use {
+  'nvim-telescope/telescope.nvim', tag = '0.1.x',
+  requires = { {'nvim-lua/plenary.nvim'} }
+}
+EOF
+}
+tetra_nvim_ls_plugins(){
+  local dir=$HOME/.local/share/nvim/site/pack/packer/start/
+  echo "ls $dir"
+  ls $dir
 }
--- tetra/bash/tmux-tetra.sh
+++ tetra/bash/tmux-tetra.sh
@@ -12,15 +12,12 @@ tetra-tmux-tetra ()
     fi
 
     tmux new-session -d -s tetra
-
     tmux setw mode-keys vi
-
-    # Split the window into two panes horizontally
-    tmux split-window -v
-
-    # Select the first pane and split it vertically
-    tmux select-pane -t 0 
-    tmux split-window -h
+    tmux split-window -v               # Step 4: Split the top pane vertically
+    tmux select-pane -t 0              # Step 3: Select the top pane
+    tmux split-window -h               # Step 2: Split the initial pane horizontally
+    tmux swap-pane -s 0 -t 2  # Swap Pane 0 (bottom) with Pane 2 (top right)
+    tmux swap-pane -s 2 -t 1  # Swap Pane 2 (now at bottom) with Pane 1 (top left)
 
     # Define hosts for panes 1 and 2, defaulting to do1 and do4_n2
     ssh1=${1:-"root@$do1"}
@@ -39,26 +36,29 @@ tetra-tmux-tetra ()
         "echo use: ssh $ssh2"
     )
 
-    pane3_commands=(
+    pane0_commands=(
         "source $HOME/$USER.sh"
         "tetra-status"
         "echo Tetra Local"
     )
 
-    # Loop through commands for pane 1 and send them thru to tmux
-    for cmd in "${pane1_commands[@]}"; do
+    # Loop through commands for pane 0 and send them thru to tmux
+    for cmd in "${pane0_commands[@]}"; do
         tmux send-keys -t 0 "$cmd" C-m
     done
+        tmux send-keys -t 0 "echo pane 0" C-m
 
-    # Loop through commands for pane 2
-    for cmd in "${pane2_commands[@]}"; do
+    # Loop through commands for pane 1 
+    for cmd in "${pane1_commands[@]}"; do
         tmux send-keys -t 1 "$cmd" C-m
     done
+        tmux send-keys -t 1 "echo pane 1" C-m
 
-    # Loop through commands for pane 3 
-    for cmd in "${pane3_commands[@]}"; do
+    # Loop through commands for pane 2 
+    for cmd in "${pane2_commands[@]}"; do
         tmux send-keys -t 2 "$cmd" C-m
     done
+        tmux send-keys -t 2 "echo pane 2" C-m
 
     # Customize status style
     tmux set mouse on
@@ -69,6 +69,43 @@ tetra-tmux-tetra ()
 
     tmux select-pane -t 2
     tmux attach-session -t tetra
+    tmux set pane-active-border-style fg=blue
+    tmux set pane-border-style fg=gray
 
 }
 
+tetra_tmux_tetra_cmd(){
+   local cmd=${2:-"clear"}
+   local pane=${1:-"1"}
+   echo using pane $pane
+   tmux send-keys -t $pane "$cmd" C-m
+}
+
+tttc(){
+  tetra_tmux_tetra_cmd 0 clear
+  tetra_tmux_tetra_cmd 1 clear
+  tetra_tmux_tetra_cmd 2 clear
+  tetra_tmux_tetra_cmd 0 "echo ttt_help for more" 
+}
+
+ttt1(){
+  tetra_tmux_tetra_cmd 1 "${@}"
+}
+ttt2(){
+  tetra_tmux_tetra_cmd 2 "${@}"
+}
+
+alias ttth='ttt_help'
+ttt_help(){
+  cat <<EOF
+ttt: tetra-tmux-tetra is a collection of functions
+     that controls multiple panes of a Tmux
+     session started by tetra-tmux-tetra.
+
+tttc: clears all panes
+tttk: kills the server
+ttt1: execute command in pane1
+ttt2: execute command in pane2
+ttt{1,2}: tetra-init
+EOF
+}
