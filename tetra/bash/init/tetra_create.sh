6739b17935886185cf15927c8315921c0938e851
Mike Ricos <mike.ricos@gmail.com>
2024-04-08 06:41:16 -0700

    Renames to tetra_create.sh

---
+++ tetra/bash/init/tetra_create.sh
@@ -0,0 +1,9 @@
+tetra_create_tetra(){
+	echo
+	echo "  You are about to blow away TETRA_DIR=$TETRA_DIR"
+	echo "  ctrl-c to quit"; read
+	echo
+
+	rm -r $TETRA_DIR 2>/dev/null
+	cp -r $TETRA_SRC/init/tetra-dir $TETRA_DIR
+}
