382525d4729bcb31fc7bfe7c2f3d64a30cf7260a
Mike Ricos <mike.ricos@gmail.com>
2024-04-08 04:14:58 -0700

    Snakecase tmux.sh

--- tetra/bash/tmux.sh
+++ tetra/bash/tmux.sh
@@ -1,28 +1,27 @@
-tetra-tmux-new(){
-  local sessionName="${1:-"untitled-session"}
-  local scriptName="${2:-"/dev/null"}
+tetra_tmux_new(){
+  local sessionName="${1:-"untitled-session"}"
+  local scriptName="${2:-"/dev/null"}"
   tmux new-session -d -s "$sessionName" "$scriptName"
 }
 
-tetra-tmux-list(){
+tetra_tmux_list(){
   tmux list-sessions # aka tmux ls
 }
 
-tetra-tmux-join () 
+tetra_tmux_join () 
 {   
     tmux has-session -t tetra 2>/dev/null &&  \
     tmux attach-session -t $1 || \
     tmux new-session -s $1
 }
 
-tetra-tmux-kill-session(){
+tetra_tmux_kill_session(){
   tmux kill-session -t $1
 }
 
-tetra-tmux-kill-server(){
+tetra_tmux_kill_server(){
   echo "Will kill everything tmux."
   tmux list-sessions
   read -p "Sure? ctrl-c to exit."
   tmux kill-server
 }
-
