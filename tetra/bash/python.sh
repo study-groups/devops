11c214286da5fa5521726bb2003c2e30674a568a
Mike Ricos <mike.ricos@gmail.com>
2024-04-12 18:23:08 -0700

    Adds python

---
+++ tetra/bash/python.sh
@@ -0,0 +1,19 @@
+tetra_python_activate_dsenv(){
+  source $TETRA_DIR/dsenv/bin/activate
+  echo "Data Science environment activated" 2>&1
+}
+
+tetra_python_create_dsenv(){
+  python3 -m venv $TETRA_DIR/dsenv # creates a Python3 virtualenv
+  echo "Data Science environment created at $TETRA_DIR/dsenv" 2>&1
+  echo "tetra_python_active_dsenv to active"
+}
+
+
+tetra_python_install_prereq(){
+  sudo apt-get install python3-dev # needed for compiling
+}
+
+tetra_python_http_serve(){
+  python -m http.server $1
+}
