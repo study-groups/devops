1ec14f4ef9d42065bc9cf713edabb0b7706bc3f4
Mike Ricos <mike.ricos@gmail.com>
2024-05-19 12:29:26 -0700

    Placeholder for drone generator

---
+++ nexus/drone/drone.sh
@@ -0,0 +1,12 @@
+drone_help(){
+  cat <<EOF
+
+  Drone is realtime signal generator geared towards 
+  binaural stereo modulation.
+
+  It runs runs a long running python process and 
+  continually accepts <truncated pico objects> and processes 
+  them to change oscilators.
+
+EOF
+}
