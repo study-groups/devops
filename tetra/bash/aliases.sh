5c93dc850d5ee0759a44d2ac791dc307cbb3c69e
Mike Ricos <mike.ricos@gmail.com>
2024-05-21 05:06:02 -0700

    Adds aliases for remote endpoints

--- tetra/bash/aliases.sh
+++ tetra/bash/aliases.sh
@@ -6,6 +6,10 @@ alias tnlr='tetra_nginx_location_replace'
 alias ttr='source $TETRA_DIR/tetra.sh'
 #alias ttr='source $TETRA_SRC/bootstrap.sh'
 
+tetra_remote_endpoint="$TETRA_REMOTE_USER@$TETRA_REMOTE:$TETRA_REMOTE_DIR"
+tetra_remote_connector="$TETRA_REMOTE_USER@$TETRA_REMOTE"
+alias tetra_remote_ls="ssh $TETRA_REMOTE_USER@$TETRA_REMOTE ls $TETRA_REMOTE_DIR"
+
 alias tna='tetra_nvm_activate'
 alias tpy='tetra_python_activate'
 
