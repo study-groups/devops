dd84ee5bc48cea40cafaa4691cf2a4385f111f9c
Mike Ricos <mricos@qa01>
2024-07-08 07:07:24 +0000

    Adds pyenv and nvm support

--- tetra/bash/python.sh
+++ tetra/bash/python.sh
@@ -10,10 +10,12 @@ tetra_python_activate_dsenv(){
   echo "Data Science environment activated" 2>&1
 }
 
-tetra_python_create_dsenv(){
+tetra_python_create_pyenv(){
+  echo "Python environment will be created at $TETRA_PYENV" 2>&1
+  echo "tetra_python_activate to active" 2>&1
+  echo "Return to continue, ctrl-c to cancel.." 2>&1
+  read
   python3 -m venv $TETRA_PYENV # creates a Python3 virtualenv
-  echo "Data Science environment created at $TETRA_PYENV" 2>&1
-  echo "tetra_python_active_dsenv to active"
 }
 
 tetra_python_run(){
@@ -22,7 +24,8 @@ tetra_python_run(){
 }
 
 tetra_python_install_prereq(){
-  sudo apt-get install python3-dev # needed for compiling
+  #sudo apt-get install python3-dev # needed for compiling
+  sudo apt install python3.10-venv
 }
 
 tetra_python_http_serve(){
