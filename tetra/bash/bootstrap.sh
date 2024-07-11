f15f6cc8df51863f4223fb5df730c45172627ecd
Mike Ricos <mricos@prod01>
2024-07-11 10:36:24 +0000

    Adds PS1 with git branch

--- tetra/bash/bootstrap.sh
+++ tetra/bash/bootstrap.sh
@@ -30,4 +30,5 @@ if command -v colima &>/dev/null && [[ $OSTYPE == 'darwin'* ]]; then
   false && colima start --arch x86_64
 fi
 
+PS1='\[\e[0;38;5;228m\]\u\[\e[0m\]@\[\e[0m\]\h\[\e[0m\]:\[\e[0;38;5;45m\][\[\e[0;38;5;45m\]\W\[\e[0;38;5;45m\]]\[\e[0;37m\](\[\e[0;37m\]$(git branch 2>/dev/null | grep '"'"'^*'"'"' | colrm 1 2)\[\e[0;37m\])\[\e[0m\]: \[\e[0m\]'
 tetra_status
