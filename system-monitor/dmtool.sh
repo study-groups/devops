919ccdf25512a6e4b755f35db66239d6a2c987d8
Mike Ricos <mricos@m2.local>
2024-08-05 04:28:38 -0700

    Re-org system-monitor

--- system-monitor/dmtool.sh
+++
@@ -1,8 +0,0 @@
-dm-create-app(){
-  echo
-  echo " This will create a directory called dmtool Requires Node.js"
-  echo " Return to continue.."
-  read # read from stdin, looks for newline
-  echo
-  npm create svelte@latest dmtool
-}
