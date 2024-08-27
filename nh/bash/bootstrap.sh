5053764108c66142d94acec8429b6f00c0b850e6
mr <mr@kingdel>
2024-08-27 01:31:45 -0700

    Path refinements

--- nh/bash/bootstrap.sh
+++ nh/bash/bootstrap.sh
@@ -1,5 +1,4 @@
 NH_DIR=${NH_DIR:-$HOME/nh}
-NH_SRC=${NH_SRC:-$HOME/src/devops/nh}
 echo " NH_DIR=$NH_DIR"
 echo " NH_SRC=$NH_SRC"
 for f in $(ls $NH_SRC/bash/*.sh | grep -v bootstrap.sh);
--- nh/init.sh
+++ nh/init.sh
@@ -1,1 +1,2 @@
+[ -z $NH_SRC ] && echo "NH_SRC not set" && return 1
 source $NH_SRC/bash/bootstrap.sh
