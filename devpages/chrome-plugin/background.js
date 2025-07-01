862391e3686245478b43e4c6e9d40a6829c55cb9
Mike Ricos <mike.ricos@gmail.com>
2025-06-30 23:27:09 -0700

    chrome-tab-exporter working

--- devpages/chrome-plugin/background.js
+++ devpages/chrome-plugin/background.js
@@ -1,13 +1,14 @@
 chrome.action.onClicked.addListener(() => {
   chrome.tabs.query({}, (tabs) => {
     const urls = tabs.map(tab => tab.url).join('\n');
-    const blob = new Blob([urls], { type: 'text/plain' });
-    const url = URL.createObjectURL(blob);
+    const encoded = encodeURIComponent(urls);
+    const dataUrl = `data:text/plain;charset=utf-8,${encoded}`;
 
     chrome.downloads.download({
-      url: url,
+      url: dataUrl,
       filename: 'tab-urls.txt',
       saveAs: true
     });
   });
 });
+
