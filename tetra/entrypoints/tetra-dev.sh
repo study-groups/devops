5c93161c6a4a2b9601bea3c32526f236c2e3cd3a
Mike Ricos <mricos@m2.local>
2025-11-15 21:09:02 -0800

    tetra-dev wip

--- tetra/entrypoints/tetra-dev.sh
+++ tetra/entrypoints/tetra-dev.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 source "$HOME/src/devops/tetra/env/dev.env"
 cd "$TETRA_SRC"
-/home/dev/tetra/nvm/versions/node/v20.19.5/bin/node server/server.js
-
+$HOME/tetra/nvm/versions/node/v20.19.5/bin/node server/server.js
